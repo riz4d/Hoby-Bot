1f784acbcf9ee7fcb2642b045424d0f51be1a7a2
Mohamed Rizad <rizadvillan@gmail.com>
2022-10-07 11:30:42 +0530

    constant!!!!!

---
+++ constant.py
@@ -0,0 +1,21 @@
+type_url='http://www.boredapi.com/api/activity?type='
+base_url='http://www.boredapi.com/api/activity/'
+
+help_msg='''
+**<u>Categories</u>**
+
+__** - /education
+- /recreational
+- /social
+- /diy
+- /charity
+- /cooking
+- /relaxation
+- /music
+- /busywork
+__**
+
+**<u>Source Code</u>**
+
+**__ - /source__**
+'''
